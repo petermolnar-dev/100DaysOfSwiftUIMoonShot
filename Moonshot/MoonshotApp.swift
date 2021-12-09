d644b658678ba4dcca0632a7a3d2fd8ce87e0ec4
Peter Molnar <petermolnar@financestack.com>
2021-12-09 09:24:29 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Peter Molnar on 09/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
