afb189a09c3394da17d931339c0c51362f061e5a
Peter Molnar <petermolnar.hu@gmail.com>
2021-12-12 17:58:15 +0100

    Mission view finalization.

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -8,7 +8,27 @@
 import SwiftUI
 
 struct MissionView: View {
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    let crew: [CrewMember]
     let mission: Mission
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map({ member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        })
+    }
+    
     
     var body: some View {
         GeometryReader { geometry in
@@ -21,10 +41,50 @@ struct MissionView: View {
                         .padding(.top)
                     
                     VStack(alignment: .leading) {
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(crew, id: \.role) { crewMember in
+                                    NavigationLink {
+                                        Text("Astronaout details")
+                                    } label: {
+                                        HStack {
+                                            Image(crewMember.astronaut.id)
+                                                .resizable()
+                                                .frame(width: 104, height: 72)
+                                                .clipShape(Capsule())
+                                                .overlay(
+                                                    Capsule()
+                                                        .strokeBorder(.white, lineWidth: 1)
+                                                )
+                                            
+                                            VStack(alignment: .leading) {
+                                                Text(crewMember.astronaut.name)
+                                                    .foregroundColor(.white)
+                                                    .font(.headline)
+                                                Text(crewMember.role)
+                                                    .foregroundColor(.secondary)
+                                            }
+                                        }
+                                        .padding(.horizontal)
+                                    }
+                                }
+                            }
+                        }
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
                         Text("Mission Highlights")
                             .font(.title.bold())
                             .padding(.bottom, 5)
                         Text(mission.description)
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundColor(.lightBackground)
+                            .padding(.vertical)
                     }
                     .padding(.horizontal)
                 }
@@ -39,9 +99,10 @@ struct MissionView: View {
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
             .preferredColorScheme(.dark)
     }
 }
