bf6d709fa61cf94ff3b9a51fa393ccdf9d7ac68f
Peter Molnar <petermolnar.hu@gmail.com>
2021-12-13 22:19:32 +0100

    Final challenge alignments.

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,52 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions.json")
-    
-    let columns = [
-        GridItem(.adaptive(minimum: 150))
-    ]
+    @State private var showGrid = false
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 200)
-                                    .padding()
-                                
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(RoundedRectangle(cornerRadius: 10)
-                                        .stroke(.lightBackground)
-                            )
-                        }
-                    }
+            Group {
+                if showGrid {
+                    MainGridView()
+                } else {
+                    MainListView()
+
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    showGrid.toggle()
+                } label: {
+                    Group {
+                        if showGrid {
+                            Image(systemName: "list.dash")
+                        } else {
+                            Image(systemName: "square.grid.2x2")
+
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ Moonshot/MainGridView.swift
@@ -0,0 +1,61 @@
+//
+//  MainGridView.swift
+//  Moonshot
+//
+//  Created by Peter Molnar on 13/12/2021.
+//
+
+import SwiftUI
+
+struct MainGridView: View {
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 200)
+                                .padding()
+                            
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                                    .foregroundColor(.white.opacity(0.5))
+                            }
+                            .padding(.vertical)
+                            .frame(maxWidth: .infinity)
+                            .background(.lightBackground)
+                        }
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                        )
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+        }
+    }
+}
+
+struct MainGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainGridView()
+    }
+}
